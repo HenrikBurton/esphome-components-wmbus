16799aaef4e322ca34a2d7ce32b559049e200728
HenrikBurton <99271512+HenrikBurton@users.noreply.github.com>
2025-03-27 20:31:57 +0100

    .

--- components/wmbus/__init__.py
+++ components/wmbus/__init__.py
@@ -132,7 +132,7 @@ async def to_code(config):
 
     var = cg.new_Pvariable(config[CONF_ID])
     await cg.register_component(var, config)
-#    await spi.register_spi_device(var, config)
+    await spi.register_spi_device(var, config)
 
 #    mosi  = await cg.gpio_pin_expression(config[CONF_MOSI_PIN])
 #    miso  = await cg.gpio_pin_expression(config[CONF_MISO_PIN])
