7b9d42fcf73936456e73628b2f5547d3c281869d
HenrikBurton <99271512+HenrikBurton@users.noreply.github.com>
2025-03-27 20:30:01 +0100

    .

--- components/wmbus/rf_sx1262.cpp
+++ components/wmbus/rf_sx1262.cpp
@@ -14,7 +14,7 @@ namespace wmbus {
     this->gdo0->setup();
     this->gdo2->setup();
     this->reset->setup();
-//    this->spi_setup();
+    this->delegate_->spi_setup();
 
     resetDevice();
     standby(RADIOLIB_SX126X_STANDBY_RC);
