1349fcb186c1995eb2d577fde24117ab4aab6130
HenrikBurton <99271512+HenrikBurton@users.noreply.github.com>
2025-04-26 16:37:53 +0200

    Update packet parameters to use 16-bit preamble detection

--- components/wmbus/rf_sx1262.cpp
+++ components/wmbus/rf_sx1262.cpp
@@ -26,7 +26,7 @@ namespace wmbus {
     setBufferBaseAddress(0x00, 0x00);
     setModulationParams(32.768f, 50.0f, 156.2f, RADIOLIB_SX126X_GFSK_FILTER_NONE); // bitrate, freqDeviation, Bandwidth, PulseShape
     setPacketParams(16, // bitlength of TX preamble
-                    RADIOLIB_SX126X_GFSK_PREAMBLE_DETECT_8, 
+                    RADIOLIB_SX126X_GFSK_PREAMBLE_DETECT_16, 
                     16, // bitlength of syncword
                     RADIOLIB_SX126X_GFSK_ADDRESS_FILT_OFF, 
                     RADIOLIB_SX126X_GFSK_PACKET_VARIABLE, 
