a0ba8bb96d349e21a0362aac916f1a73bf7a1d18
HenrikBurton <99271512+HenrikBurton@users.noreply.github.com>
2025-04-26 15:58:48 +0200

    Fix setRxGain function to use correct register for setting receiver gain

--- components/wmbus/rf_sx1262.cpp
+++ components/wmbus/rf_sx1262.cpp
@@ -371,7 +371,7 @@ namespace wmbus {
   }
 
   void setRxGain(uint8_t gain) {
-    uint8_t command[] = { RADIOLIB_SX126X_CMD_SET_RX_GAIN, gain };
+    uint8_t command[] = { RADIOLIB_SX126X_REG_RX_GAIN, gain };
     sx1262command(command, sizeof(command));
   }
 
