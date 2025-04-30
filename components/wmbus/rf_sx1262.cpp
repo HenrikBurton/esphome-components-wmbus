721d1cb581cba9eaa75aac533c25363a71e8b630
HenrikBurton <99271512+HenrikBurton@users.noreply.github.com>
2025-04-30 15:48:23 +0200

    Update sync word length in setPacketParams from 16 to 15 bits and modify sync word values in setSyncWord function

--- components/wmbus/rf_sx1262.cpp
+++ components/wmbus/rf_sx1262.cpp
@@ -28,7 +28,7 @@ namespace wmbus {
     //setModulationParams(32.768f, 50.0f, 156.2f, RADIOLIB_SX126X_GFSK_FILTER_NONE); // bitrate, freqDeviation, Bandwidth, PulseShape
     setPacketParams(16, // bitlength of TX preamble
                     RADIOLIB_SX126X_GFSK_PREAMBLE_DETECT_16, 
-                    16, // bitlength of syncword
+                    15, // bitlength of syncword
                     RADIOLIB_SX126X_GFSK_ADDRESS_FILT_OFF, 
                     RADIOLIB_SX126X_GFSK_PACKET_VARIABLE, 
                     0xff,
@@ -476,7 +476,8 @@ namespace wmbus {
     uint8_t command[] = { 
       RADIOLIB_SX126X_CMD_WRITE_REGISTER,
       RADIOLIB_SX126X_REG_SYNC_WORD_0 >> 8 & 0xff, RADIOLIB_SX126X_REG_SYNC_WORD_0  & 0xff,
-      0x54, 0x3d, 0x00, 0x00, 0x00, 0x00
+      //0x54, 0x3d, 0x00, 0x00, 0x00, 0x00
+      0xa8, 0x7a, 0x00, 0x00, 0x00, 0x00
     };
     sx1262command(command, sizeof(command));
   }
