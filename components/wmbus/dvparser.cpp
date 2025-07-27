0368deec75202af0b7d7d86df7b0a2d6bb5d641a
HenrikBurton <99271512+HenrikBurton@users.noreply.github.com>
2025-07-27 13:38:34 +0200

    Rolled back sprintf to strprintf

--- components/wmbus/dvparser.cpp
+++ components/wmbus/dvparser.cpp
@@ -1426,11 +1426,11 @@ debug("[DVPARSER]");
         int count = ++dv_count[dv];
 debug("[DVPARSER] dv = %s, count = %d", dv.c_str(), count);
         if (count > 1) {
-            sprintf(&key, "%s_%d", dv.c_str(), count);
+            strprintf(&key, "%s_%d", dv.c_str(), count);
         }
         else {
 debug("[DVPARSER]");
-            sprintf(&key, "%s", dv.c_str());
+            strprintf(&key, "%s", dv.c_str());
         }
 debug("[DVPARSER]");
         DEBUG_PARSER("(dvparser debug) DifVif key is %s", key.c_str());
